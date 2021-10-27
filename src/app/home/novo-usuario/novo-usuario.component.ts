c8aec8e878b104634fa25550ab347b3b61d6759d
PedroAugustoCR <pedroaugusto_cr@hotmail.com>
2021-10-27 20:58:27 -0300

    Implements service cadastrar

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from '../models/novo-usuario';
 import { minusculoValidator } from 'src/app/validators/minusculo.validator';
+import { usuarioSenhaIguaisValidator } from 'src/app/validators/usuarioSenhaIguaisValidator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -14,7 +16,8 @@ export class NovoUsuarioComponent implements OnInit {
 
   novoUsuarioForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private NovoUsuarioService: NovoUsuarioService, private usuarioExistenteService: UsuarioExisteService) { }
+  constructor(private formBuilder: FormBuilder, private NovoUsuarioService: NovoUsuarioService,
+    private usuarioExistenteService: UsuarioExisteService, private router: Router) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
@@ -22,12 +25,22 @@ export class NovoUsuarioComponent implements OnInit {
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: ['', [minusculoValidator], [this.usuarioExistenteService.usuarioJaExiste()]],
       password: [''],
-    });
+    },
+      {
+        validators: [usuarioSenhaIguaisValidator],
+      });
   }
 
   cadastrar() {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+    if (this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.NovoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(() => {
+        this.router.navigate(['']);
+      },
+        error => {
+          console.log(error)
+        });
+    }
   }
 
 }
